42424acac8fc7a3fec71ed71d0bf57e0f1ed729b
Michaela Stefkova <37817290+michaelastefkova@users.noreply.github.com>
2021-09-29 11:35:12 +0100

    Update _layout.svelte

--- source/routes/_layout.svelte
+++ source/routes/_layout.svelte
@@ -1,1 +1,16 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	import NavSticker from '../components/NavSticker.svelte';
+	import Footer from '../components/Footer.svelte';
+	export let segment;
+</script>
 
+<style>
+</style>
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
+
+<Footer/>
